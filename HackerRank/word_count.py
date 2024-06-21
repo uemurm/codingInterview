cd29732c6a3da4f7bec17b56bb19850eb434e8d0
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2024-06-21 13:25:54 +1000

    Solve test case 0.

--- HackerRank/word_count.py
+++ HackerRank/word_count.py
@@ -1,10 +1,58 @@
+import sys
+
+ALPHABETS = 'abcdefghijklmnopqrstuvwxyz'
+VOWELS = 'aeiou'
+CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
+
 def is_valid_word(word):
-    return word
+    if len(word) < 3:
+        if debug:
+            print('Less than 3 chars')
+        return False
+    if not alphanumeric(word):
+        if debug:
+            print('non alpha-numeric')
+        return False
+
+    return has_vowel(word) and has_consonant(word)
+
+def has_vowel(word):
+    for vowel in VOWELS:
+        if vowel in word.lower():
+            return True
+    if debug:
+        print('without vowels')
+    return False
+
+def has_consonant(word):
+    for consonant in CONSONANTS:
+        if consonant in word.lower():
+            return True
+    if debug:
+        print('without consonants')
+    return False
+
+def alphanumeric(word):
+    word = word.lower()
+    for ch in list(word):
+        if ch not in '0123456789' and \
+           ch not in ALPHABETS and \
+           ch not in ALPHABETS.upper():
+            return False
+    return True
+
+def list_words(string):
+    for word in string.split():
+        print(is_valid_word(word), '\t', end='')
+        print(word)
 
 def countValidWords(string):
     valid_words = [word for word in string.split() if is_valid_word(word)]
     return len(valid_words)
 
+debug = '-d' or '--debug' in sys.argv
+
 s = input()
-result = countValidWords(s)
-print(result)
+list_words(s)
+# result = countValidWords(s)
+# print(result)
