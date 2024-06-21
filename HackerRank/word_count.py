97c0195b1f3ed66fa40bda6afb3297dd405ee74b
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2024-06-21 17:29:24 +1000

    Use regex and isalpha(). Add more test cases and count those words.

--- HackerRank/word_count.py
+++ HackerRank/word_count.py
@@ -2,60 +2,59 @@
 Count valid words.
 """
 import sys
+import re
 
-ALPHABETS = 'abcdefghijklmnopqrstuvwxyz'
-VOWELS = 'aeiou'
-CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
+VOWELS = 'aeiouAEIOU'
 
 def is_valid_word(word):
-    if len(word) < 3:
-        if debug:
-            print('Less than 3 chars')
-        return False
-    if not alphanumeric(word):
-        if debug:
-            print('non alpha-numeric')
+    """
+    Determine a valid word.
+    """
+    if not re.match(r'^[a-zA-Z0-9]{3,}$', word):
         return False
 
-    return has_vowel(word) and has_consonant(word)
+    return has_vowel_and_consonant(word)
 
-def has_vowel(word):
-    for vowel in VOWELS:
-        if vowel in word.lower():
+def has_vowel_and_consonant(word):
+    """
+    Determine if the word has both a vowel and a consonant.
+    """
+    has_vowel = False
+    has_consonant = False
+
+    for ch in word:
+        if ch.isalpha():
+            if ch in VOWELS:
+                has_vowel = True
+            else:
+                has_consonant = True
+        if has_vowel and has_consonant:
             return True
-    if debug:
-        print('without vowels')
     return False
-
-def has_consonant(word):
-    for consonant in CONSONANTS:
-        if consonant in word.lower():
-            return True
-    if debug:
-        print('without consonants')
-    return False
-
-def alphanumeric(word):
-    word = word.lower()
-    for ch in list(word):
-        if ch not in '0123456789' and \
-           ch not in ALPHABETS and \
-           ch not in ALPHABETS.upper():
-            return False
-    return True
 
 def list_words(string):
     for word in string.split():
         print(is_valid_word(word), '\t', end='')
         print(word)
+    print()
 
-def countValidWords(string):
+def count_valid_words(string):
+    """
+    The method in question.
+    """
     valid_words = [word for word in string.split() if is_valid_word(word)]
     return len(valid_words)
 
 debug = '--debug' in sys.argv or \
              '-d' in sys.argv
 
-# list_words(input())
-result = countValidWords(input())
-print(result)
+while True:
+    try:
+        line = input()
+        if debug:
+            list_words(line)
+        else:
+            result = count_valid_words(line)
+            print(result)
+    except EOFError:
+        break
